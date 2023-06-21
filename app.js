bda1feccdb5daa6b0c97d0d52b922a04d8142d70
Víctor Peña <victortkd88@gmail.com>
2023-06-20 19:54:07 -0600

    files

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-const [values1, values2] = process.argv.slice(2);
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(path.join(__dirname, 'files/hello.csv'), 'utf-8', (error, data) => {
+  if (error) throw error;
+
+  const lines = data.split('\n');
+  const objects = [];
+
+  for (const line of lines) {
+    const [name, lastName, age] = line.split(',');
+
+    objects.push({ name, lastName, age });
+  }
+
+  console.log(objects);
+});
+
+fs.writeFile(path.join(__dirname, 'files/test.csv'), 'torvi rap22', (err) => {
+  if (err) throw new err;
+
+  console.log('Funcionó');
+});
+
+process.on('uncaughtException', (err) => {
+  console.log('Paso algo!', err);
+  process.exit(1);
+})
