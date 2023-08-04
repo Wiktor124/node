41903975954ea0ef17163aa98edbc3886d18f765
Víctor Peña Ramírez <victortkd88@gmail.com>
2023-08-03 21:21:44 -0600

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const path = require('path')
-const testRoutes = require('./routes/routes');
-
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, './public')))
-app.use(testRoutes)
+app.use(express.text());
+
+app.get('/', (req, res) => {
+  res.send('¡Hola, bienvenido a mi servidor Express!');
+});
+
+app.get('/saludo/:nombre', (req, res) => {
+  const { nombre } = req.params.nombre;
+  res.send(`Hola, ${nombre}!`);
+});
+
+app.post('/post', (req, res) => {
+  console.log('post', req.body)
+  res.send('Se hizo un post.');
+});
+
+app.use((req, res) => {
+  res.status(404).send('Ruta no encontrada');
+});
 
 app.listen(PORT, () => {
   console.log(`Server on port ${PORT}`);
