ece05333f484140ab3cf40f7b22f530bb8188a88
Víctor Peña Ramírez <victortkd88@gmail.com>
2023-08-08 19:58:02 -0600

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
-const path = require('path')
-const testRoutes = require('./routes/routes');
-
+const db = require('./db')
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, './public')))
-app.use(testRoutes)
+
+app.get('/', (req, res) => {
+  res.status(200).send('¡Bienvenido al servidor HTTP usando Express!')
+})
+
+app.get('/task', (req, res) => {
+  res.status(200).json(db)
+})
+
+app.post('/task', (req, res) => {
+  // const { name, description } = req.body;
+  console.log(req.body);
+
+  // db.push({ id, name, description });
+  res.status(201).json({ message: 'Tarea agregada exitosamente.' });
+});
+
+// app.get('/image/:username', (req, res) => {
+//   const { username } = req.params;
+//   if (username === 'ronny') {
+//     return res.sendFile(__dirname + '/ronny_image.jpg');
+//   } else {
+//     return res.status(404).send('Página no encontrada');
+//   }
+// });
+
+app.use((req, res) => {
+  res.status(404).send('Página no encontrada');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server on port ${PORT}`);
